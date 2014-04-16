44a78108f9605424911dbf620b852bcc71a8bc71
yaelgoodman <yaeligoodman@gmail.com>
2014-04-16 15:51:47 +0300

    commit1 adding missing files

---
+++ VideebackDemo/Scripts/app.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('groupPageApp', ["ngRoute", "ngResource"]);
+
+app.factory('UserProfile', function ($resource) {
+    return $resource('/api/UserProfile/:id', { id: '@id' }, { update: { method: 'PUT' } });
+});
+
+app.controller('videoDivController', function ($scope) {
+    $scope.closeVideo = function (state) {
+        document.getElementById("dvShowVideo").style.display = "none";
+    }
+});
+
+app.controller('userProfileController', function ($scope, $location, UserProfile) {
+    $scope.UserPofiles = UserProfile.query();
+    $scope.expandYouTube = function (code) {
+        $scope.code = code;
+        var updateSrc = "http://www.youtube.com/embed/" + code;
+
+        document.getElementById("iframeYouTubeVid").src = updateSrc;
+
+        document.getElementById("dvShowVideo").style.display = "inline";
+    }
+});
+
+function toggleVideo(state) {
+    var div = document.getElementById("popupVid");
+    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+    div.style.display = state == 'hide' ? 'none' : '';
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+}
